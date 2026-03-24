9ad341026b3db155daf3d541f95195cc13afbc24
Reddy-PLK <kvashishtreddy@gmail.com>
2026-03-25 00:48:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');  
+
 const app = express();
 
+app.use(cors());  
+
 app.get('/api', (req, res) => {
-    res.json({ message: "Hello from Local Backend 🚀" });
+    res.json({ message: "Hello from Local Backend" });
 });
 
 app.listen(3000, () => {
