7324a05f5d613da22642b3e7bfed5bf063a02f6f
Reddy-PLK <kvashishtreddy@gmail.com>
2026-03-25 00:00:29 +0530

    Update server.js

--- backend /server.js
+++ backend /server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
 
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from Local Backend 🚀" });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
